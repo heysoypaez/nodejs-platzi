787d7bb5e60b6fc3053d5cec9f7314c81bd6f44a
Daniel Páez <danielpaezsw66@gmail.com>
2019-10-02 15:30:03 -0300

    Test added for routes and services

--- platzi-backend-node/movies-api/utils/test/routes.movies.test.js
+++ platzi-backend-node/movies-api/utils/test/routes.movies.test.js
@@ -1,18 +1,30 @@
 const assert = require("assert");
 const proxyquire = require("proxyquire");
 const { moviesMock, MoviesServiceMock } = require("../mocks/movies.js");
-const testServer = require("../utils/testServer.js");
+const testServer = require("../testServer.js");
 
 describe("Routes - movies", function() {
 	const route = proxyquire("../../routes/movies.js", {
-		"../../services/movies.js": MoviesServiceMock
+		"../services/movies.js": MoviesServiceMock
 	});
+
+	//When we call the service Movie who we will really serve is MoviesServiceMock
 
 	const request = testServer(route);
 
 	describe("GET /movies", function() {
 		it("should respond with status 200", function(done) {
 			request.get("/api/movies").expect(200, done);
+		});
+
+		it("should respond with the list of movies", function(done) {
+			request.get("/api/movies").end((err, res) => {
+				assert.deepEqual(res.body, {
+					data: moviesMock,
+					message: "movies listed"
+				});
+				done();
+			});
 		});
 	});
 });
